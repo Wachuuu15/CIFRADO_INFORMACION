81f2a7db14403c3bc41d16bec7ca90eca199556e
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2025-05-15 18:04:18 -0600

    init exercise

---
+++ ejercicio_qkd.py
@@ -0,0 +1,9 @@
+## Ejercicio QKD
+
+## Objetivos
+# Que los estudiantes comprendan a fondo cómo funciona BB84, 
+# incluyendo el rol del “lanzamiento de moneda”, y puedan simularlo en papel o en 
+# código para identificar claves seguras y ataques potenciales
+
+# Instrucciones
+
